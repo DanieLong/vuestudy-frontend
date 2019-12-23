a74d817986b55463a21a92ba5652d2ab14283869
daniel <mdaniell0318@gmail.com>
2019-12-23 21:17:26 +0800

    add tableComponets

---
+++ src/components/TableComponets.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <el-table :data="data" stripe style="width: 100%">
+      <el-table-column type="index" width="50" label="序号"></el-table-column>
+      <div v-for="t in title" :key="t.label">
+        <el-table-column :prop="t.prop" :label="t.label"></el-table-column>
+      </div>
+      <el-table-column label="操作">
+        <el-button-group>
+          <template slot-scope="scope">
+            <el-container>
+              <div v-if="role['update']">
+                <el-button type="primary" icon="el-icon-edit" circle @click="edit(scope.$row)"></el-button>
+              </div>
+              <div v-if="role['del']">
+                <el-button type="danger" icon="el-icon-delete" circle @click="del(scope.$row)"></el-button>
+              </div>
+            </el-container>
+          </template>
+        </el-button-group>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  //简单一行表格
+  name: 'tableComponets',
+  components: {},
+  props: {
+    tableInit: {
+      //列信息
+      title: Array,
+      //内容信息
+      tableData: Array,
+      role: Object
+    }
+  },
+  data() {
+    return {
+      title: [],
+      data: [],
+      role: {}
+    }
+  },
+  created() {
+    // let colmnus = this.table.coulmnData
+    // for (let c of colmnus) {
+    //   console.log(c)
+    // }
+  },
+  mounted() {
+    this.initData()
+  },
+  methods: {
+    initData: function() {
+      this.title = this.tableInit.title
+      this.data = this.tableInit.tableData
+      this.role = this.tableInit.role
+    },
+
+    //日期格式化
+    dateFomatter: function() {},
+    //数字格式化
+    numberFomatter: function() {},
+    //编辑数据
+    edit: function(row) {
+      console.log(row)
+      return row
+    },
+    //删除数据
+    delData: function(row) {
+      console.log(row)
+      return row
+    }
+  }
+}
+</script>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
-    <h1>123456</h1>
-    <el-table :data="tableData" stripe style="width: 100%">
+    <!-- <el-table :data="tableData" stripe style="width: 100%">
       <el-table-column type="index" width="50" label="序号"></el-table-column>
       <el-table-column prop="username" label="用户名" width="180"></el-table-column>
       <el-table-column prop="nickname" label="昵称" width="180"></el-table-column>
       <el-table-column prop="lastLoginTime" label="上次登录时间"></el-table-column>
-      <el-table-column prop="“userStatus”" label="状态" :formatter="statusFomatter"></el-table-column>
+      <el-table-column prop="userStatus" label="状态" :formatter="statusFomatter"></el-table-column>
       <el-table-column label="操作">
         <el-button type="primary" icon="el-icon-edit" circle></el-button>
         <el-button type="danger" icon="el-icon-delete" circle></el-button>
       </el-table-column>
-    </el-table>
-    <pagination :pageModel="pageModel" />
+    </el-table>-->
+    <div>
+      <!-- 查询条件 每个页面查询条件都不太相似 不好提出来公用组件 -->
+      <el-container>
+        <div class="queryCondition"></div>
+      </el-container>
+    </div>
+    <tableComponets :tableInit="table1" ref="tableComponets" />
+    <pagination :pageModel="pageModel" ref="pagination" />
   </div>
 </template>
 
@@ -20,10 +26,11 @@
 // import { UserService } from '../services/user'
 import { Auth } from '../tools/auth'
 import axios from 'axios'
+import tableComponets from '../components/TableComponets.vue'
 import pagination from '../components/Pagination.vue'
 export default {
   name: 'user',
-  components: { pagination },
+  components: { tableComponets, pagination },
   data() {
     return {
       user: {},
@@ -35,10 +42,20 @@ export default {
         currentPage: 1,
         url: '',
         args: ''
+      },
+      table1: {
+        role: { update: true, del: true },
+        title: [
+          { label: '用户名', prop: 'username' },
+          { label: '昵称', prop: 'nickname' },
+          { label: '上次登录时间', prop: 'lastLoginTime' },
+          { label: '状态', prop: 'userStatus' }
+        ],
+        tableData: []
       }
     }
   },
-  created() {
+  mounted() {
     this.getUserList()
   },
   methods: {
@@ -53,6 +70,7 @@ export default {
           for (let i = 0; i < data.length; i++) {
             // console.log(data[i])
             this.tableData.push(data[i])
+            this.table1.tableData.push(data[i])
           }
         })
         .catch(err => {
@@ -75,4 +93,8 @@ export default {
 </script>
 
 <style >
+.queryCondition {
+  margin-top: 5%;
+  background-color: azure;
+}
 </style>
